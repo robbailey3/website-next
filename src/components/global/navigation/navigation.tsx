f6728acf10cb25cb917ba9bfc38e23214bd3720c
Rob Bailey <rob.bailey3@gmail.com>
2021-10-21 12:06:24 +0100

    build: updating lint & prettier config

--- src/components/global/navigation/navigation.tsx
+++ src/components/global/navigation/navigation.tsx
@@ -5,79 +5,76 @@ import { fromEvent, map, Subscription, throttleTime } from 'rxjs';
 import clsx from 'clsx';
 
 const Navigation = () => {
-	const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-	const [currentWindowSize, setCurrentWindowSize] = useState(0);
+  const [currentWindowSize, setCurrentWindowSize] = useState(0);
 
-	useEffect(() => {
-		let windowResizeSubscription: Subscription;
-		if (typeof window !== 'undefined') {
-			setCurrentWindowSize(window.innerWidth);
-			windowResizeSubscription = fromEvent(window, 'resize')
-				.pipe(
-					throttleTime(500),
-					map(
-						($event: Event) =>
-							($event.target as Window).innerWidth || 0
-					)
-				)
-				.subscribe({
-					next: (windowSize) => {
-						setCurrentWindowSize(windowSize);
-					},
-				});
-		}
-		return () => {
-			if (windowResizeSubscription) {
-				windowResizeSubscription.unsubscribe();
-			}
-		};
-	});
+  useEffect(() => {
+    let windowResizeSubscription: Subscription;
+    if (typeof window !== 'undefined') {
+      setCurrentWindowSize(window.innerWidth);
+      windowResizeSubscription = fromEvent(window, 'resize')
+        .pipe(
+          throttleTime(500),
+          map(($event: Event) => ($event.target as Window).innerWidth || 0)
+        )
+        .subscribe({
+          next: (windowSize) => {
+            setCurrentWindowSize(windowSize);
+          },
+        });
+    }
+    return () => {
+      if (windowResizeSubscription) {
+        windowResizeSubscription.unsubscribe();
+      }
+    };
+  }, []);
 
-	const toggleNavigation = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleNavigation = () => {
+    setIsOpen(!isOpen);
+  };
 
-	const isMobileDevice = () => currentWindowSize < 768;
+  const isMobileDevice = () => currentWindowSize < 768;
 
-	return (
-		<nav
-			className={clsx(styles.navigation, {
-				[styles.navigation__open]: isOpen || !isMobileDevice(),
-			})}
-		>
-			{isMobileDevice() && (
-				<button onClick={toggleNavigation}>Toggle Navigation</button>
-			)}
-			<ul>
-				<li>
-					<Link href="/">
-						<a>Home</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/">
-						<a>About</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/github">
-						<a>GitHub</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/">
-						<a>Projects</a>
-					</Link>
-				</li>
-				<li>
-					<Link href="/">
-						<a>CV</a>
-					</Link>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav
+      className={clsx(styles.navigation, {
+        [styles.navigation__open]: isOpen || !isMobileDevice(),
+      })}
+    >
+      {isMobileDevice() && (
+        <button onClick={toggleNavigation}>Toggle Navigation</button>
+      )}
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>About</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/github">
+            <a>GitHub</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Projects</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>CV</a>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
--- src/pages/github/index.tsx
+++ src/pages/github/index.tsx
@@ -6,33 +6,35 @@ import { GetUserResponse } from '@/services/github/responses/GetUserResponse';
 import React, { useEffect, useState } from 'react';
 
 const GitHubPage = () => {
-	const [repos, setRepos] = useState<GetUserRepositoriesResponse[]>();
-	const [user, setUser] = useState<GetUserResponse>();
+  const [repos, setRepos] = useState<GetUserRepositoriesResponse[]>();
+  const [user, setUser] = useState<GetUserResponse>();
 
-	const [username, setUsername] = useState('robbailey3');
+  const [username, setUsername] = useState('robbailey3');
 
-	useEffect(() => {
-		const fetchRepos = async () => {
-			let result = await githubService.GetUserRepositories(username, { per_page: 25 });
-			setRepos(result);
-		};
-		const fetchUser = async () => {
-			let result = await githubService.GetUser(username);
-			setUser(result);
-		};
-		fetchRepos();
-		fetchUser();
-	}, []);
+  useEffect(() => {
+    const fetchRepos = async () => {
+      let result = await githubService.GetUserRepositories(username, {
+        per_page: 25,
+      });
+      setRepos(result);
+    };
+    const fetchUser = async () => {
+      let result = await githubService.GetUser(username);
+      setUser(result);
+    };
+    fetchRepos();
+    fetchUser();
+  }, [username]);
 
-	if (!repos || !user) {
-		return <div>Loading...</div>;
-	}
-	return (
-		<div className="github-page">
-			<GithubUserDetails user={user} />
-			<GithubRepositories repos={repos} />
-		</div>
-	);
+  if (!repos || !user) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="github-page">
+      <GithubUserDetails user={user} />
+      <GithubRepositories repos={repos} />
+    </div>
+  );
 };
 
 export default GitHubPage;
