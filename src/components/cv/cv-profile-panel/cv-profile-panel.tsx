0facbe1d583025b92351af81e4ad9e30917717d3
Rob Bailey <rob.bailey3@gmail.com>
2021-10-22 14:50:11 +0100

    build: adding stuff

--- src/components/cv/cv-profile-panel/cv-profile-panel.tsx
+++ src/components/cv/cv-profile-panel/cv-profile-panel.tsx
@@ -1,4 +1,7 @@
 import { CV } from '@/data/cv/cv';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { motion } from 'framer-motion';
+import React from 'react';
 
 const CVProfilePanel = () => {
   const { profile } = CV;
@@ -8,7 +11,31 @@ const CVProfilePanel = () => {
       <h2>
         {profile.firstName} {profile.lastName}
       </h2>
+      <div>
+        <p>
+          {profile.location.city}, {profile.location.country}
+        </p>
+      </div>
       <p>{profile.profile}</p>
+      <div>
+        {profile.socialMedia.map((social, i) => (
+          <motion.div
+            key={social.name}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: i * 0.2 }}
+          >
+            <FontAwesomeIcon icon={social.icon} />
+            <a
+              href={social.url.toString()}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {social.name}
+            </a>
+          </motion.div>
+        ))}
+      </div>
     </section>
   );
 };
--- src/components/github/github-repositories/github-repositories.tsx
+++ src/components/github/github-repositories/github-repositories.tsx
@@ -2,39 +2,49 @@ import Container from '@/components/common/layout/container/container';
 import FlexContainer from '@/components/common/layout/flex-container/flex-container';
 import FlexItem from '@/components/common/layout/flex-item/flex-item';
 import { GetUserRepositoriesResponse } from '@/services/github/responses/GetUserRepositoriesResponse';
+import { motion } from 'framer-motion';
 import React from 'react';
 import GithubRepositoryItem from '../github-repository-item/github-repository-item';
 import styles from './github-repositories.module.scss';
 
 type GithubRepositoriesProps = {
-	repos: GetUserRepositoriesResponse[];
+  repos: GetUserRepositoriesResponse[];
 };
 
 const GithubRepositories = (props: GithubRepositoriesProps) => {
-	const { repos } = props;
-	return (
-		<section data-cy="github-repositories" className={styles.repos}>
-			<Container>
-				<h2>Repositories</h2>
-				<FlexContainer
-					options={{
-						wrap: 'wrap',
-						justify: 'space-around',
-					}}
-				>
-					{repos.map((repo) => (
-						<FlexItem
-							key={repo.id}
-							className={styles.repo_item}
-							options={{ basis: 'calc(50% - 1rem)' }}
-						>
-							<GithubRepositoryItem repo={repo} />
-						</FlexItem>
-					))}
-				</FlexContainer>
-			</Container>
-		</section>
-	);
+  const { repos } = props;
+  return (
+    <motion.section
+      data-cy="github-repositories"
+      className={styles.repos}
+      initial={{ y: -10, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 3 }}
+    >
+      <Container>
+        <h2>Repositories</h2>
+        <FlexContainer
+          options={{
+            wrap: 'wrap',
+            justify: 'space-around',
+          }}
+        >
+          {repos.map((repo, i) => (
+            <FlexItem key={repo.id} options={{ basis: 'calc(50% - 1rem)' }}>
+              <motion.div
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.3, delay: i * 0.1 }}
+                className={styles.repo_item}
+              >
+                <GithubRepositoryItem repo={repo} />
+              </motion.div>
+            </FlexItem>
+          ))}
+        </FlexContainer>
+      </Container>
+    </motion.section>
+  );
 };
 
 export default GithubRepositories;
