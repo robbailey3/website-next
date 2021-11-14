1c3065122c1aa14400c729fa2049bd171f7454c3
Rob Bailey <rob.bailey3@gmail.com>
2021-11-14 19:38:31 +0000

    test: adding a couple of modal tests

---
+++ src/components/common/modal/modal.spec.tsx
@@ -0,0 +1,31 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Modal from './modal';
+import styles from './modal.module.scss';
+
+describe('[COMPONENT]: Modal', () => {
+  const createWrapper = (props: any) => {
+    return shallow(<Modal {...props} />);
+  };
+  it('should display the backdrop when the modal state is "open"', () => {
+    const wrapper = createWrapper({
+      state: 'open',
+    });
+    expect(wrapper.find(`.${styles.modal__backdrop}`)).toHaveLength(1);
+  });
+
+  it('should apply the "open" attribute to the HTML Dialog element when the state is "open"', () => {
+    const wrapper = createWrapper({
+      state: 'open',
+    });
+    expect(wrapper.find(`.${styles.modal__dialog}`).prop('open')).toBe(true);
+  });
+
+  it('should render the title in a H2 element in the modal', () => {
+    const wrapper = createWrapper({
+      state: 'open',
+      title: 'Test title',
+    });
+    expect(wrapper.find('h2').text()).toBe('Test title');
+  });
+});
