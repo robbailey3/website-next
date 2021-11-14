6600af3e6039a061be1dc832b75e03594a5145c8
Rob Bailey <rob.bailey3@gmail.com>
2021-11-14 19:30:50 +0000

    build: adding keyboard shortcut handler

--- src/components/common/modal/modal.tsx
+++ src/components/common/modal/modal.tsx
@@ -1,3 +1,41 @@
-const Modal = () => {};
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import clsx from 'clsx';
+import { motion } from 'framer-motion';
+import React from 'react';
+import styles from './modal.module.scss';
+
+type ModalProps = {
+  children?: JSX.Element | JSX.Element[];
+  title: string;
+  state: 'open' | 'closed';
+  className?: string;
+  onClose: () => void;
+};
+
+const Modal = (props: ModalProps) => {
+  const { children, title, state, onClose, className } = props;
+
+  return (
+    <>
+      <div className={clsx(styles.modal, className)}>
+        {state === 'open' && (
+          <div className={styles.modal__backdrop} onClick={onClose}></div>
+        )}
+        <dialog open={state === 'open'} className={styles.modal__dialog}>
+          <header>
+            <h2>{title}</h2>
+            <button className={styles.modal__close} onClick={onClose}>
+              <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+            </button>
+          </header>
+          <div>{children}</div>
+        </dialog>
+      </div>
+    </>
+  );
+};
 
 export default Modal;
