f6042d59f559b4394784cc85f16195fcd7eafaaf
Rob Bailey <rob.bailey3@gmail.com>
2021-11-17 13:39:48 +0000

    build: fixing cypress navbar tests

--- cypress/integration/global/navigation.spec.ts
+++ cypress/integration/global/navigation.spec.ts
@@ -61,13 +61,30 @@ describe('[GLOBAL COMPONENT]: Navigation', () => {
     it('should contain links to the different pages', () => {
       cy.get('[data-cy=navigation]').within(() => {
         cy.get('a').should('have.length', 5);
-        cy.get('a').should('contain', 'Home').and('have.attr', 'href', '/');
-        cy.get('a').should('contain', 'About').and('have.attr', 'href', '/');
         cy.get('a')
+          .eq(0)
+          .should('contain', 'Home')
+          .and('have.attr', 'href', '/');
+
+        cy.get('a')
+          .eq(1)
+          .should('contain', 'About')
+          .and('have.attr', 'href', '/');
+
+        cy.get('a')
+          .eq(2)
           .should('contain', 'GitHub')
           .and('have.attr', 'href', '/github');
-        cy.get('a').should('contain', 'Projects').and('have.attr', 'href', '/');
-        cy.get('a').should('contain', 'CV').and('have.attr', 'href', '/cv');
+
+        cy.get('a')
+          .eq(3)
+          .should('contain', 'Projects')
+          .and('have.attr', 'href', '/');
+
+        cy.get('a')
+          .eq(4)
+          .should('contain', 'CV')
+          .and('have.attr', 'href', '/cv');
       });
     });
   });
