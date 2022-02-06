9225e6f62736c075d2708b0a5050a3aea61672fb
Rob Bailey <rob.bailey3@gmail.com>
2022-02-06 21:26:39 +0000

    build: updating loader logic

--- src/components/common/Loaders/PageLoader/PageLoader.tsx
+++ src/components/common/Loaders/PageLoader/PageLoader.tsx
@@ -9,15 +9,23 @@ const PageLoader = () => {
   const [loading, setLoading] = useState(false);
 
   React.useEffect(() => {
-    router.events.on('routeChangeStart', () => {
-      setLoading(true);
-    });
-    router.events.on('routeChangeComplete', () => {
-      setLoading(false);
-    });
-    router.events.on('routeChangeError', () => {
-      setLoading(false);
-    });
+    const handleStart = (url: string) => {
+      url !== router.pathname && setLoading(true);
+    };
+
+    const handleComplete = (url: string) => {
+      url === router.pathname && setLoading(false);
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
   });
 
   return loading ? (
