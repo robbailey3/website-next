3a73fc92afbf2a66aff994b2e9e2c0b0a9ecd312
Rob Bailey <rob.bailey3@gmail.com>
2022-02-07 20:36:13 +0000

    build: adding test to improve coverage

--- src/features/projects/components/ProjectList/ProjectList.spec.tsx
+++ src/features/projects/components/ProjectList/ProjectList.spec.tsx
@@ -12,6 +12,10 @@ describe('[COMPONENT]: ProjectList', () => {
     createWrapper();
   });
 
+  it('should contain a heading with "Projects" in it', () => {
+    expect(wrapper.find('h1').render().text()).toEqual('Projects');
+  });
+
   it('should render a ProjectListItem for each project in the data', () => {
     expect(wrapper.find('ProjectListItem').length).toBe(projects.length);
   });
