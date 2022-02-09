719dc47a246612f0dcc149b78fd2296c12eb454a
Rob Bailey <rob.bailey3@gmail.com>
2022-02-09 20:14:31 +0000

    build: updating header

--- src/components/global/Header/Header.spec.tsx
+++ src/components/global/Header/Header.spec.tsx
@@ -7,13 +7,20 @@ describe('[COMPONENT]: Header', () => {
     wrapper = shallow(<Header />);
   };
 
-  it('should contain my name in a span element', () => {
+  beforeEach(() => {
     createWrapper();
+  });
+
+  it('should contain a link back to the homepage', () => {
+    console.log(wrapper.debug());
+    expect(wrapper.find('Link').prop('href')).toBe('/');
+  });
+
+  it('should contain my name in a span element', () => {
     expect(wrapper.find('span').first().text()).toEqual('Rob Bailey');
   });
 
   it('should contain "software engineer" in another span element', () => {
-    createWrapper();
     expect(wrapper.find('span').at(1).text()).toEqual('Software Engineer');
   });
 });
