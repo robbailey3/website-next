b91fe8e80a2e089d4c39ae80ad04d78b97f0752d
Rob Bailey <rob.bailey3@gmail.com>
2022-02-17 21:54:09 +0000

    Adding title and meta

--- src/pages/projects/word-game.tsx
+++ src/pages/projects/word-game.tsx
@@ -1,11 +1,21 @@
 import Container from '@/components/common/Container/Container';
 import WordGame from '@/features/word-game/components/WordGame/WordGame';
+import Head from 'next/head';
 
 const WordGamePage = () => {
   return (
-    <Container>
-      <WordGame />
-    </Container>
+    <>
+      <Head>
+        <title>Word Game / Projects / Rob Bailey</title>
+        <meta
+          name="description"
+          content="A clone of a particular word game which is very popular at the moment"
+        />
+      </Head>
+      <Container>
+        <WordGame />
+      </Container>
+    </>
   );
 };
 
