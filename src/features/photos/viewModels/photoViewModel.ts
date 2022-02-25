30bf7b843b7a00755cb0eeb71727fb18eddaa9a9
Rob Bailey <rob.bailey3@gmail.com>
2022-02-25 20:44:51 +0000

    build: adding new lazy image component

--- src/features/photos/viewModels/photoViewModel.ts
+++ src/features/photos/viewModels/photoViewModel.ts
@@ -1,6 +1,7 @@
 export interface PhotoViewModel {
   _id: string;
   url: string;
+  thumbnailUrl: string;
   albumId: string;
   caption: string;
   createdAt: Date;
--- src/pages/admin/photos/[id].tsx
+++ src/pages/admin/photos/[id].tsx
@@ -1,5 +1,7 @@
+import LazyImage from '@/components/common/LazyImage/LazyImage';
 import usePhotoAlbum from '@/features/photos/hooks/usePhotoAlbum';
 import usePhotos from '@/features/photos/hooks/usePhotos';
+import { PhotoViewModel } from '@/features/photos/viewModels/photoViewModel';
 import { useRouter } from 'next/router';
 
 const PhotoAlbumPage = () => {
@@ -12,18 +14,32 @@ const PhotoAlbumPage = () => {
   const photosResponse = usePhotos(id as string);
 
   return (
-    <pre>
-      {JSON.stringify(
-        {
-          album: albumsResponse.album,
-          photos: photosResponse.photos,
-          error: albumsResponse.error || photosResponse.error,
-          isLoading: albumsResponse.isLoading || photosResponse.isLoading,
-        },
-        null,
-        4
-      )}
-    </pre>
+    <>
+      {photosResponse &&
+        photosResponse.photos &&
+        photosResponse.photos.map((photo: PhotoViewModel) => (
+          <div key={photo._id} className="w-full">
+            <LazyImage
+              src={photo.url}
+              thumbnailSrc={photo.thumbnailUrl}
+              alt={photo.caption}
+              className="h-full w-full object-cover"
+            />
+          </div>
+        ))}
+      <pre>
+        {JSON.stringify(
+          {
+            album: albumsResponse.album,
+            photos: photosResponse.photos,
+            error: albumsResponse.error || photosResponse.error,
+            isLoading: albumsResponse.isLoading || photosResponse.isLoading,
+          },
+          null,
+          4
+        )}
+      </pre>
+    </>
   );
 };
 
