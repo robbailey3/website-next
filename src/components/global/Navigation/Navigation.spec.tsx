7a1b541e22a63aa350e9cffd8507ed74337f7a68
Rob Bailey <rob.bailey3@gmail.com>
2022-03-05 19:30:07 +0000

    build: fixing test

--- src/components/global/Navigation/Navigation.spec.tsx
+++ src/components/global/Navigation/Navigation.spec.tsx
@@ -1,10 +1,15 @@
+import { UserProvider } from '@auth0/nextjs-auth0';
 import { ShallowWrapper, shallow } from 'enzyme';
 import Navigation from './Navigation';
 
 describe('[COMPONENT]: Navigation', () => {
   let wrapper: ShallowWrapper<typeof Navigation>;
   const createWrapper = () => {
-    wrapper = shallow(<Navigation />);
+    wrapper = shallow(
+      <UserProvider>
+        <Navigation />
+      </UserProvider>
+    );
   };
 
   it('should render without crashing', () => {
