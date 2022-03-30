791ad462e9ad862c03cd883b9f5934dbd8a6f1a7
Rob Bailey <rob.bailey3@gmail.com>
2022-03-30 20:16:53 +0100

    build: removing some console logs

--- src/features/photos/components/AdminUploadingPhoto/AdminUploadingPhoto.tsx
+++ src/features/photos/components/AdminUploadingPhoto/AdminUploadingPhoto.tsx
@@ -1,6 +1,7 @@
 import Loader from '@/components/common/Loaders/Loader/Loader';
-import TaskQueue, { TaskItem } from '@/utils/TaskQueue';
+import TaskQueue, { TaskItem, TaskItemStatus } from '@/utils/TaskQueue';
 import axios from 'axios';
+import clsx from 'clsx';
 import React from 'react';
 
 export interface AdminUploadingPhotoProps {
@@ -14,6 +15,12 @@ const AdminUploadingPhoto = (props: AdminUploadingPhotoProps) => {
 
   const [isLoading, setIsLoading] = React.useState(false);
 
+  const [taskStatus, setTaskStatus] = React.useState<TaskItemStatus | null>(
+    null
+  );
+
+  const [uploadProgress, setUploadProgress] = React.useState(0);
+
   const uploadPhoto = React.useCallback(async () => {
     const taskItem = new TaskItem(async () => {
       const formdata = new FormData();
@@ -22,19 +29,18 @@ const AdminUploadingPhoto = (props: AdminUploadingPhotoProps) => {
 
       await axios.post(`/api/photos/${albumId}/upload`, formdata, {
         onUploadProgress: (progressEvent) => {
-          const percentCompleted = Math.round(
-            (progressEvent.loaded * 100) / progressEvent.total
+          const percentCompleted = Math.min(
+            95,
+            Math.round((progressEvent.loaded * 100) / progressEvent.total)
           );
 
-          console.log({ percentCompleted });
+          setUploadProgress(percentCompleted);
         },
       });
+      setUploadProgress(100);
     });
-    taskItem.on('started', () => {
-      console.log('started', `${file.name} started`);
-    });
-    taskItem.on('completed', () => {
-      setIsLoading(false);
+    taskItem.status.subscribe((status) => {
+      setTaskStatus(status);
     });
     TaskQueue.addTask(taskItem);
   }, [file, albumId]);
@@ -47,13 +53,27 @@ const AdminUploadingPhoto = (props: AdminUploadingPhotoProps) => {
   return (
     <div className="w-48 h-48 mr-4 mb-4 relative">
       {isLoading && (
-        <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-black bg-opacity-40">
+        <div
+          className={clsx(
+            'absolute top-0 left-0 h-full w-full flex justify-center items-center bg-black',
+            {
+              'bg-opacity-75': taskStatus === 'pending',
+              'bg-opacity-40': taskStatus !== 'pending',
+            }
+          )}
+        >
           <Loader />
         </div>
       )}
       {previewSrc && (
         <img src={previewSrc} alt="" className="w-full h-full object-cover" />
       )}
+      <div className="absolute bottom-0 left-0 h-1 w-full">
+        <div
+          className="h-1 bg-green-500 rounded-full duration-1000"
+          style={{ width: `${uploadProgress}%` }}
+        />
+      </div>
     </div>
   );
 };
--- src/utils/TaskQueue.ts
+++ src/utils/TaskQueue.ts
@@ -1,55 +1,50 @@
 import {
+  BehaviorSubject,
   catchError,
-  flatMap,
   from,
-  map,
   mergeMap,
+  Observable,
   of,
   Subject,
   tap,
 } from 'rxjs';
 
-export type TaskItemStatus = 'started' | 'completed' | 'failed';
-
-export class TaskItemEvent extends Event {}
+export type TaskItemStatus = 'pending' | 'started' | 'completed' | 'failed';
 
 export class TaskItem {
-  public completed = new Subject<TaskItemEvent>();
-  public started = new Subject<TaskItemEvent>();
-  public failed = new Subject<TaskItemEvent>();
+  public status = new BehaviorSubject<TaskItemStatus>('pending');
 
-  constructor(public run: () => Promise<any>) {}
+  constructor(public task: () => Promise<any>) {}
 
-  public on(event: TaskItemStatus, callback: () => void): void {
-    this[event].subscribe(callback);
+  public runTask(): Observable<any> {
+    this.status.next('started');
+    return from(this.task()).pipe(
+      catchError((error) => {
+        this.status.next('failed');
+        return of(error);
+      }),
+      tap(() => this.status.next('completed'))
+    );
   }
 }
 
 export class TaskQueue {
   private queue: Subject<TaskItem> = new Subject();
 
-  private isRunning = false;
-
-  private MAX_CONCURRENT_TASKS = 2;
+  private MAX_CONCURRENT_TASKS = 1;
 
   constructor() {
     this.queue
       .pipe(
         mergeMap((item) => {
-          item.started.next(new TaskItemEvent(''));
-          return from(item.run()).pipe(map(() => item));
+          return item.runTask();
         }, this.MAX_CONCURRENT_TASKS)
       )
-      .subscribe({
-        next: (item) => {
-          item.completed.next(new TaskItemEvent(''));
-        },
-      });
+      .subscribe();
   }
 
   public addTask(item: TaskItem) {
     this.queue.next(item);
-    console.log('task added', this.queue);
   }
 }
 export default new TaskQueue();
