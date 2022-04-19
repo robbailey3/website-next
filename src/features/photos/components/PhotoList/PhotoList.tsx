14f4fb8fb5a1f513d58a5614ebd2f15b09588963
Rob Bailey <rob.bailey3@gmail.com>
2022-04-19 21:24:27 +0100

    Update photo display to split by date Fixes #241

--- src/features/photos/components/PhotoList/PhotoList.tsx
+++ src/features/photos/components/PhotoList/PhotoList.tsx
@@ -7,12 +7,42 @@ export interface PhotoListProps {
   photos: PhotoModel[];
 }
 
+export interface PhotoGroup {
+  title: string;
+  photos: PhotoModel[];
+}
+
 const PhotoList = (props: PhotoListProps) => {
   const { photos } = props;
 
   const [activePhoto, setActivePhoto] = React.useState<PhotoModel | undefined>(
     undefined
   );
+
+  const [photoGroups, setPhotoGroups] = React.useState<PhotoGroup[]>([]);
+
+  const getPhotoMonthGroups = React.useCallback((): PhotoGroup[] => {
+    return photos.reduce((acc, photo) => {
+      const title = new Date(photo.updatedAt!).toLocaleDateString('en-GB', {
+        month: 'long',
+        year: 'numeric',
+      });
+      const existingGroup = acc.find((group: any) => group.title === title);
+      if (existingGroup) {
+        existingGroup.photos.push(photo);
+      } else {
+        acc.push({
+          title,
+          photos: [photo],
+        });
+      }
+      return acc;
+    }, [] as PhotoGroup[]);
+  }, [photos]);
+
+  React.useEffect(() => {
+    setPhotoGroups(getPhotoMonthGroups());
+  }, [photos, getPhotoMonthGroups]);
 
   if (!photos) {
     return null;
@@ -49,13 +79,23 @@ const PhotoList = (props: PhotoListProps) => {
   };
 
   return (
-    <div className="flex mt-8 flex-wrap justify-between">
-      {photos.map((photo) => (
-        <Photo
-          photo={photo}
-          key={photo._id as string}
-          onClick={() => handlePhotoClick(photo)}
-        />
+    <div className="mt-8">
+      {photoGroups.map((photoGroup) => (
+        <>
+          <h3 className="my-4 border-b border-b-gray-100">
+            {photoGroup.title}
+          </h3>
+          <div className="flex flex-wrap">
+            {photoGroup.photos.map((photo) => (
+              <div
+                key={photo._id as string}
+                className="w-full md:w-1/2 xl:w-1/3 p-4"
+              >
+                <Photo photo={photo} onClick={() => handlePhotoClick(photo)} />
+              </div>
+            ))}
+          </div>
+        </>
       ))}
       {activePhoto && (
         <FullScreenPhoto
