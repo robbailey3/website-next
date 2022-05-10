83911c270f9ab9fb26ecb0de979f1fa03b9706fe
Rob Bailey <rob.bailey3@gmail.com>
2022-05-10 19:58:03 +0100

    Add pokemon page titles Fixes #263

--- src/pages/projects/pokemon/index.tsx
+++ src/pages/projects/pokemon/index.tsx
@@ -2,15 +2,21 @@ import Container from '@/components/common/Container/Container';
 import PokemonList from '@/features/pokemon/components/PokemonList/PokemonList';
 import { Species } from '@/features/pokemon/models/species';
 import { SpeciesListing } from '@/features/pokemon/models/speciesListing';
+import Head from 'next/head';
 import React from 'react';
 
 const PokemonPage = () => {
   return (
-    <Container>
-      <h1>Pokemon Page</h1>
-      <p>This is the pokemon page.</p>
-      <PokemonList />
-    </Container>
+    <>
+      <Head>
+        <title>Pokemon App / Projects / Rob Bailey</title>
+      </Head>
+      <Container>
+        <h1>Pokemon Page</h1>
+        <p>This is the pokemon page.</p>
+        <PokemonList />
+      </Container>
+    </>
   );
 };
 
